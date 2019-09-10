be097a08e25e23b4d688c88109709659186c4cd0
frozanna <48160361+frozanna@users.noreply.github.com>
2019-09-10 15:33:15 +0200

    More cpp look

---
+++ lab4/cmake-build-debug/CMakeFiles/cpp4.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp4.dir/Punkt.cpp.obj"
+  "CMakeFiles/cpp4.dir/Figura.cpp.obj"
+  "CMakeFiles/cpp4.dir/Kolo.cpp.obj"
+  "CMakeFiles/cpp4.dir/Prostokat.cpp.obj"
+  "CMakeFiles/cpp4.dir/Kwadrat.cpp.obj"
+  "CMakeFiles/cpp4.dir/main.cpp.obj"
+  "cpp4.pdb"
+  "cpp4.exe"
+  "cpp4.exe.manifest"
+  "libcpp4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
